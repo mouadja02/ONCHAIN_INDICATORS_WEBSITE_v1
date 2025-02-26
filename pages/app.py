9915f92bce692efcfab4439eb7ed1b537be3b8b7
MJ <mouadpro02@gmail.com>
2025-02-26 12:32:51 +0100

    update

---
+++ pages/app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from my_draw_component import st_my_draw_component
+
+st.set_page_config(layout="wide")
+
+st.title("My Custom Drawable Plotly Component")
+
+# Provide data or arguments to your component
+shapes_data = st_my_draw_component(
+    x=[1,2,3,4],
+    y=[10,15,5,12],
+    key="draw1"
+)
+
+st.write("Shapes (relayout data) returned from the front-end:")
+st.json(shapes_data)
--- pages/my_draw_component/my_draw_component.py
+++ pages/my_draw_component/my_draw_component.py
@@ -1,29 +1,21 @@
 import os
 import streamlit.components.v1 as components
 
-# If True, render the component in "development mode" (localhost).
-# If False, render from the built frontend within the 'build' folder.
-_RELEASE = False
+_RELEASE = True  # <-- set to True for production
 
 if not _RELEASE:
-    # Development (hot reload) mode
     _component_func = components.declare_component(
         "my_draw_component",
         url="http://localhost:3000"
     )
 else:
-    # Production mode: path to the build folder
     build_dir = os.path.join(os.path.dirname(__file__), "frontend", "build")
-    _component_func = components.declare_component("my_draw_component", path=build_dir)
-
+    _component_func = components.declare_component(
+        "my_draw_component", 
+        path=build_dir
+    )
 
 def st_my_draw_component(x=None, y=None, key=None):
-    """
-    Wrapper function to call our custom drawing component.
-    x, y => data arrays to plot
-    Returns the shape events from the front-end if any.
-    """
-    # We pass the data to the front end as "args"
     component_value = _component_func(
         x=x,
         y=y,
